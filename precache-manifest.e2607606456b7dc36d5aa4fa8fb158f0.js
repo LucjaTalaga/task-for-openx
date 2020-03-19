f953b0dbb19c7a509a20db2ac84dc08d74ab1d00
Piotr Talaga <piotrek.talaga@gmail.com>
2020-03-19 21:32:40 +0100

    Updates

---
+++ precache-manifest.e2607606456b7dc36d5aa4fa8fb158f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4131166c041fb333a9a5b402438017",
+    "url": "/task-for-openx/index.html"
+  },
+  {
+    "revision": "b9b0ec21cf10191d20fb",
+    "url": "/task-for-openx/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81c7777338c08c7c8a3b",
+    "url": "/task-for-openx/static/js/2.dd007138.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/task-for-openx/static/js/2.dd007138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b0ec21cf10191d20fb",
+    "url": "/task-for-openx/static/js/main.06d26e36.chunk.js"
+  },
+  {
+    "revision": "1daec7d53469bd37b8c8",
+    "url": "/task-for-openx/static/js/runtime-main.05c49736.js"
+  }
+]);
